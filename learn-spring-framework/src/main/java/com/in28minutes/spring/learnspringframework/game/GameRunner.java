68a10771a40935c7b9151140cd828e0c6250cb79
yonaswoldemichael <ybyonas@gmail.com>
2022-11-17 11:03:05 -0500

    update

---
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/game/GameRunner.java
@@ -0,0 +1,22 @@
+package com.in28minutes.spring.learnspringframework.game;
+
+public class GameRunner {
+	
+//	private MarioGame game;
+	private SuperContraGame game;
+
+	public GameRunner(SuperContraGame game) {
+		this.game = game;
+		
+	}
+
+	public void run() {
+		game.up();
+		game.down();
+		game.left();
+		game.right();
+		
+		
+	}
+
+}
