9793b27f92d8ff4b76005a5c6724da9e27f4accb
yonaswoldemichael <ybyonas@gmail.com>
2022-12-01 09:00:18 -0500

    Added PUT/Update CRUD operation

--- learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/course/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/learnspringboot/course/controller/CourseController.java
@@ -7,6 +7,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.learnspringboot.course.bean.Course;
@@ -28,7 +31,7 @@ public class CourseController {
 //		return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"),new Course(2, "Learn Full Stack w React", "in28minutes"));
 	}
 
-	
+//	GET - Retrieve information (/courses, /courses/1)
 	// http://localhost:8080/courses/1
 	@GetMapping("/courses/{id}")
 	public Course getCourseDetails(@PathVariable long id) {
@@ -43,9 +46,17 @@ public class CourseController {
 //		return new Course(1, "Learn Microservices 1", "in28minutes");
 	}
 	
-//	GET - Retrieve information (/courses, /courses/1)
 //	POST - Create a new resource (/courses)
+	@PostMapping("/courses")
+	public void createCourse(@RequestBody Course course) {
+		repository.save(course);
+
+	}
 //	PUT - Update/Replace a resources (/courses/1)
+	@PutMapping("/courses/{id}")
+	public void updateCourse(@PathVariable long id, @RequestBody Course course) {
+		repository.save(course);
+	}
 //	DELETE - Delete a resource (/courses/1)
 	
 }
